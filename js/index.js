1cb422be3805bd036f67b3e12e7bbcbf87c3fe86
MRosales0390 <marco_rosales@hotmail.es>
2022-07-15 01:01:34 -0500

    Added changes for retrieving data from backend

--- js/index.js
+++ js/index.js
@@ -10,11 +10,13 @@ let post = {
     avatarAuthor: 'https://res.cloudinary.com/practicaldev/image/fetch/s--3xRt7osW--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/395121/4dd73e99-88c7-4886-b485-cd246beaaf92.jpg'
 }
 */
+const server_url = "http://localhost:8080/"
+
 const getFormattedDate = (postOriginalDate) => {
-  let currentDate = new Date();
-  let postDate = new Date(postOriginalDate);
+  let currentDate = new Date()
+  let postDate = new Date(postOriginalDate)
   //let monthShortNameList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  let printDate = "";
+  let printDate = ""
 
   if (
     currentDate.getFullYear() != postDate.getFullYear() ||
@@ -23,39 +25,40 @@ const getFormattedDate = (postOriginalDate) => {
   ) {
     //printDate = `${monthShortNameList[postDate.getMonth()]} ${postDate.getDay()} '${}`
     printDate = `${postDate.toLocaleString("en-US", {
-      month: "short",
-    })} ${postDate.getDate()} '${postDate.getFullYear().toString().slice(-2)}`;
+      month: "short"
+    })} ${postDate.getDate()} '${postDate.getFullYear().toString().slice(-2)}`
   } else {
     printDate = `${postDate.toLocaleString("en-US", {
-      month: "short",
+      month: "short"
     })} ${postDate.getDate()} (${
       currentDate.getHours() != postDate.getHours()
         ? currentDate.getHours() - postDate.getHours() + " hour(s) ago"
         : currentDate.getMinutes() - postDate.getMinutes() + " minute(s) ago"
-    })`;
+    })`
   }
 
-  return printDate;
-};
+  return printDate
+}
 
 const getTagsList = (listOfTags) => {
-  let formattedTags = "";
-  if (getTagsList) {
-    let tagsArray = listOfTags.split(",");
+  console.log(listOfTags)
+  let formattedTags = ""
+  if (listOfTags) {
+    //let tagsArray = listOfTags.split(",")
 
-    formattedTags = tagsArray.reduce((tagsList, currentTag) => {
-      tagsList += `#${currentTag.trim()} `;
+    formattedTags = listOfTags.reduce((tagsList, currentTag) => {
+      tagsList += `#${currentTag.trim()} `
 
-      return tagsList;
-    }, "");
+      return tagsList
+    }, "")
   }
 
-  return formattedTags;
-};
+  return formattedTags
+}
 
 const getCardTemplate = (post, currentId = 0, includeImageOnHeader = false) => {
-  let printDate = getFormattedDate(post.createdDate);
-  let tagsList = getTagsList(post.tags);
+  let printDate = getFormattedDate(post.createdDate)
+  let tagsList = getTagsList(post.tags)
 
   let postLayout = `
       <div class="card mb-3">
@@ -103,143 +106,100 @@ const getCardTemplate = (post, currentId = 0, includeImageOnHeader = false) => {
             </div>
           </div>
       </div>
-  `;
+  `
 
-  return postLayout;
-};
+  return postLayout
+}
 
 const getPosts = (filter = "") => {
   return new Promise(function (resolve, reject) {
-    fetch("https://devto-clone-team3-default-rtdb.firebaseio.com/posts/.json", {
+    fetch(server_url, {
       method: "GET",
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
+        "Content-type": "application/json; charset=UTF-8"
+      }
     })
       .then((response) => {
         if (!response.ok) {
           let err = new Error(
             `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}`
-          );
-          throw err;
+          )
+          throw err
         } else {
-          return response.json();
+          return response.json()
         }
       })
       .then((posts) => {
-        let postsLayout = "";
-        let firstPost = true;
-        let topPosts = {};
+        let postsLayout = ""
+        let firstPost = true
+        let topPosts = {}
 
         if (filter === "") {
-          topPosts = posts;
+          topPosts = posts
         } else {
           topPosts = Object.fromEntries(
             Object.entries(posts).filter(([key, value]) => {
-              return value.tags.replaceAll(" ", "").split(",").includes(filter);
+              return value.tags.replaceAll(" ", "").split(",").includes(filter)
             })
-          );
+          )
         }
 
         for (post in topPosts) {
-          postsLayout += getCardTemplate(posts[post], post, firstPost);
-          firstPost = false;
+          postsLayout += getCardTemplate(posts[post], post, firstPost)
+          firstPost = false
         }
 
-        resolve(postsLayout);
+        resolve(postsLayout)
       })
       .catch((err) => {
-        console.log(err);
-      });
-  });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://devto-clone-team3-default-rtdb.firebaseio.com/posts/.json", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
+        console.log(err)
+      })
   })
-    .then((response) => {
-      if (!response.ok) {
-        let err = new Error(
-          `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}`
-        );
-        throw err;
-      } else {
-        return response.json();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch(server_url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
       }
     })
-    .then((posts) => {
-      let postsLayout = "";
-      let relevantPostsSection = document.getElementById("relevant");
-      let firstPost = true;
 
-      for (post in posts) {
-        postsLayout += getCardTemplate(posts[post], post, firstPost);
-        firstPost = false;
-      }
+    const jsonObject = await response.json()
+    const posts = jsonObject.data.getPosts
+    console.log(posts)
 
-      relevantPostsSection.innerHTML = postsLayout;
+    let postsLayout = ""
+    let relevantPostsSection = document.getElementById("relevant")
+    let firstPost = true
+
+    posts.forEach((post, index) => {
+      postsLayout += getCardTemplate(post, post._id, firstPost)
+      firstPost = false
     })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+
+    relevantPostsSection.innerHTML = postsLayout
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 document
   .querySelector("#top-tab")
   .addEventListener("shown.bs.tab", function () {
-    let topPostsSection = document.getElementById("top");
+    let topPostsSection = document.getElementById("top")
     let test = getPosts("top").then(
       (response) => (topPostsSection.innerHTML = response)
-    );
-    /*
-    fetch("https://devto-clone-team3-default-rtdb.firebaseio.com/posts/.json", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          let err = new Error(
-            `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}`
-          );
-          throw err;
-        } else {
-          return response.json();
-        }
-      })
-      .then((posts) => {
-        let postsLayout = "";
-        let topPostsSection = document.getElementById("top");
-        let firstPost = true;
-
-        const topPosts = Object.fromEntries(
-          Object.entries(posts).filter(([key, value]) => {
-            return value.tags.replaceAll(" ", "").split(",").includes("top");
-          })
-        );
-
-        for (post in topPosts) {
-          postsLayout += getCardTemplate(posts[post], post, firstPost);
-          firstPost = false;
-        }
-
-        topPostsSection.innerHTML = postsLayout;
-      })
-      .catch((err) => {
-        console.log(err);
-      });*/
-  });
+    )
+  })
 
 document
   .querySelector("#latest-tab")
   .addEventListener("shown.bs.tab", function () {
-    let topPostsSection = document.getElementById("latest");
+    let topPostsSection = document.getElementById("latest")
     let test = getPosts("latest").then(
       (response) => (topPostsSection.innerHTML = response)
-    );
-  });
+    )
+  })
